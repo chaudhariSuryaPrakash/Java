52f623a1def352301e2a68b2052c6f8f2a6f80d1
chaudharisuryaprakash <123prem.monster@gmail.com>
2025-07-20 00:26:07 +0530

    push from main2

--- Aboutme.java
+++ Aboutme.java
@@ -3,7 +3,7 @@ public class Aboutme {
         System.out.println("Hello, my name is Prem.");
         System.out.println("I am a software developer with a passion for coding.");
         System.out.println("I enjoy working on open-source projects and contributing to the community.");
-        System.out.println("In my free time, I like to explore new technologies.");
+        System.out.println("In my free time, I like to explore new technologiess.");
     }
 }
 
