2a24d264957ea925952c1a83eda5f911a29a7e4d
chaudharisuryaprakash <123prem.monster@gmail.com>
2025-07-21 11:58:42 +0530

    21/07/2025 cw

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,20 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int x = 3;
+        int y = 4;
+        System.out.println("Before swapping:");
+        System.out.println("x" + " = " + x);
+        System.out.println("y" + " = " + y);
+        int sum;
+        // sum = x + y; // Calculate the sum of x and y
+        sum = x;
+        x= y;
+        y = sum;
+        
+        System.out.println("After swapping:");
+        System.out.println("x" + " = " + x);
+        System.out.println("y" + " = " + y);
+        // System.out.println("The sum of " + x + " and " + y + " is: "  +sum);
     }
 }
